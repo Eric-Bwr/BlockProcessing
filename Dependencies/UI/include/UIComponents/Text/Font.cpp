ba1b3f56c53fd9bdd080a7fb05774bd923ee9c1f
Eric Böwer <e.boewer@kalverkamp.de>
2022-09-29 07:53:32 +0200

    Removed Deps

--- Dependencies/UI/include/UIComponents/Text/Font.cpp
+++
@@ -1,9 +0,0 @@
-#include "Font.h"
-
-Font::Font(const char* path, int faceIndex) : path(path) {
-    errorCode = FT_New_Face(DataManager::library, path, faceIndex, &face);
-}
-
-Font::~Font() {
-    FT_Done_Face(face);
-}
